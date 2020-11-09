ba721c8d714861f1486bc97b08c19bf21e813eab
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-09 18:51:35 +0100

    Created methods to start, stop and reset stopwatch. Also created method to start counting and increment timer.

--- app/src/main/java/com/linuxkyrios/stopwatch/MainActivity.java
+++ app/src/main/java/com/linuxkyrios/stopwatch/MainActivity.java
@@ -1,14 +1,59 @@
 package com.linuxkyrios.stopwatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super .onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        goTimer();
+    }
+
+    //This method activate stopwatch when button start is pressed
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    //This method stops watch when stop button is pressed
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    //This method resets stopwatch when reset button is pressed
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void goTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        //Creating new handler instance
+        final Handler handler = new Handler();
+        //This method contains new Runnable object. Post method executes code instantly
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secon = seconds%60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secon);
+                //This instruction displays text in TextView component
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
